baff2f0b1782790490b975cb0f24066c8b91516f
Zsolt Harmath <harmathzs@gmail.com>
2025-11-03 14:23:12 +0100

    Implement lwc methods

--- force-app/main/default/lwc/myFirstLightningCard/myFirstLightningCard.js
+++ force-app/main/default/lwc/myFirstLightningCard/myFirstLightningCard.js
@@ -6,4 +6,12 @@ export default class MyFirstLightningCard extends LightningElement {
     connectedCallback() {
         console.log('connectedCallback recordId', this.recordId)
     }
+
+    renderedCallback() {
+        console.log('renderedCallback recordId', this.recordId)
+    }
+
+    disconnectedCallback() {
+        console.log('disconnectedCallback recordId', this.recordId)
+    }
 }
