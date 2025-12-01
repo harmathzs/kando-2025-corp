4ff03175162c31bf97ab02918c13d2ca60363fb0
Zsolt Harmath <harmathzs@gmail.com>
2025-12-01 13:37:43 +0100

    Put LWC to page

--- force-app/main/default/lwc/leadConverterLwc/leadConverterLwc.js
+++ force-app/main/default/lwc/leadConverterLwc/leadConverterLwc.js
@@ -1,12 +1,6 @@
+/** leadConverterLwc */
 import { LightningElement } from "lwc";
 
-import customConvertLead from '@salesforce/apex/LeadConversionService.customConvertLead';
-
 export default class LeadConverterLwc extends LightningElement {
-  leadId;
-  result;
-
-  async convertLead() {
-    this.result = await customConvertLead({leadId: this.leadId})
-  }
+  
 }
