c7bf740672bf3b8b73a4bdd36da56a6e1d150e67
Zsolt Harmath <harmathzs@gmail.com>
2025-12-01 13:50:40 +0100

    Add recordId

--- force-app/main/default/lwc/leadConverterLwc/leadConverterLwc.js
+++ force-app/main/default/lwc/leadConverterLwc/leadConverterLwc.js
@@ -1,6 +1,19 @@
 /** leadConverterLwc */
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
 export default class LeadConverterLwc extends LightningElement {
-  
+  @api recordId;
+
+  connectedCallback() {
+    console.log('LeadConverterLwc connectedCallback')
+  }
+
+  renderedCallback() {
+    console.log('LeadConverterLwc renderedCallback')
+    console.log('renderedCallback recordId', this.recordId)
+  }
+
+  disconnectedCallback() {
+    console.log('LeadConverterLwc disconnectedCallback')
+  }
 }
