8b0c6ebe044718e810e741b8ca1d2623c3e26f73
Zsolt Harmath <harmathzs@gmail.com>
2026-03-31 14:36:55 +0200

    Prepare for displaying total price with VAT in component

--- force-app/main/default/lwc/showTotalPriceWithVat/showTotalPriceWithVat.js
+++ force-app/main/default/lwc/showTotalPriceWithVat/showTotalPriceWithVat.js
@@ -1,5 +1,14 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
 
 export default class ShowTotalPriceWithVAT extends LightningElement {
+  totalPriceWithVAT;
 
+  _recordId;
+  @api get recordId() {
+    return this._recordId;
+  }
+  set recordId(value) {
+    this._recordId = value;
+    console.log('recordId', this._recordId)
+  }
 }
