1c64b4e3150c2c3a2e668319e5afc58e6060ddb4
Zsolt Harmath <harmathzs@gmail.com>
2026-04-21 13:51:44 +0200

    Implement Contract PDF screen with recordId handling and PDF URL generation

--- force-app/main/default/lwc/contractPdfScreenLwc/contractPdfScreenLwc.js
+++ force-app/main/default/lwc/contractPdfScreenLwc/contractPdfScreenLwc.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ContractPdfScreenLwc extends LightningElement {}
+export default class ContractPdfScreenLwc extends LightningElement {
+    _contractId;
+    pdfUrl;
+
+    @api get recordId() {
+        return this._contractId
+    }
+    set recordId(value) {
+        this._contractId = value
+        console.log('set recordId ', this.recordId)
+
+        this.pdfUrl = '/apex/ContractPDF?id='+this.recordId
+    }
+
+}
